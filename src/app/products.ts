dc9d16b49035b52e151d5a188ae4a54f9b3afdbf
Vasile Drozdovschi <vaseav57@gmail.com>
2021-07-27 23:10:12 +0300

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products = [ 
+    {
+ id: 1,
+ name: 'Samsung G20',
+ price: 999,
+ description: 'This is a brand new Samsung G20 smartphone'
+},
+{
+ id: 2,
+ name: 'Samsung G20S',
+ price: 1299,
+ description: 'This is a brand new Samsung G20S smartphone'
+},
+{
+id: 1,
+ name: 'Samsung Galaxy Tab',
+ price: 1399,
+ description: 'This is a brand new Samsung Galaxy Tab'
+},
+]
